977d4056fc41a3651341ac0f648b33cd30d2ff3d
rmuliterno <renan-mf1@hotmail.com>
2022-05-07 21:14:09 -0300

    first commit

---
+++ src/components/Copyright/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+import { styles } from './styles';
+
+// parei em 1:01:50 
+
+export function Copyright() {
+  return (
+    <Text style={styles.text}>
+      Feito com ♥ junto com a Rocketseat
+    </Text>
+  );
+}
